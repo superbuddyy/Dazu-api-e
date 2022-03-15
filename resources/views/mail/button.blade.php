f9a949ab4668915287c11f114af606aedc5a7d1d
Vijay White <whitedevil3244@gmail.com>
2022-03-15 22:47:53 +0530

    mail updates

---
+++ resources/views/mail/button.blade.php
@@ -0,0 +1,19 @@
+<table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="center">
+<table border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<a href="{{ $url }}" class="button button-{{ $color ?? '' }}" target="_blank" rel="noopener" style="background-color: #ff19b7;border-bottom: 8px solid #ff19b7;border-left: 18px solid #ff19b7;border-right: 18px solid #ff19b7;border-top: 8px solid #ff19b7;">{{ $slot }}</a>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
