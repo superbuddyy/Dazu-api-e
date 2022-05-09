24a22bde41053cf24bb35c0621a6b50a021cd41c
Vijay White <whitedevil3244@gmail.com>
2022-05-09 23:04:34 +0530

    updates

--- app/Http/Controllers/Api/Client/OfferController.php
+++ app/Http/Controllers/Api/Client/OfferController.php
@@ -235,7 +235,7 @@ class OfferController
             $status = $offer->status;
             echo $offer->title;
             echo $offer->description;
-            echo ($offer->title != strip_tags($request->get('title'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || ($offer->description != strip_tags($request->get('description'),'<b><strong><em><u><br><p><i><ul><li><ol>'));
+            echo (($offer->title != strip_tags($request->get('title'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || ($offer->description != strip_tags($request->get('description'),'<b><strong><em><u><br><p><i><ul><li><ol>')));
             echo "<br>";
             echo "end========================111";
             if (($offer->title != strip_tags($request->get('title'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || ($offer->description != strip_tags($request->get('description'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || $request->has('images')) {
