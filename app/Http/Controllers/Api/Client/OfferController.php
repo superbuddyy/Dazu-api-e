93618a0dffc43020976f629bb87b8eba58e208b1
Vijay White <whitedevil3244@gmail.com>
2022-05-12 22:47:52 +0530

    updates

--- app/Http/Controllers/Api/Client/OfferController.php
+++ app/Http/Controllers/Api/Client/OfferController.php
@@ -234,6 +234,7 @@ class OfferController
         try {
             dump($offer->links);
             dump($request->get('links', []));
+            dump($offer->links != $request->get('links', []));
             $status = $offer->status;
             if (($offer->title != strip_tags($request->get('title'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || ($offer->description != strip_tags($request->get('description'),'<b><strong><em><u><br><p><i><ul><li><ol>')) || $request->has('images')) {
                 $status = OfferStatus::PENDING;
