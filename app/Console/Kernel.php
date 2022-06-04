cc024d6bf89a7c23370b5ba8d8322e235a829b6d
Artur <artur@5andhalf.com>
2022-06-04 18:06:38 +0200

    Fix scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule-command('ads:status')->hourly();
-        $schedule-command('user:delete')->daily();
+        $schedule->command('ads:status')->hourly();
+        $schedule->command('user:delete')->daily();
     }
 
     /**
