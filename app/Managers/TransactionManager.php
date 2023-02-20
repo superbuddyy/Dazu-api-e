ff31ef16b61f6b4b349fc9a93548c3ebe8681a5e
nubby0208 <109526476+gruTonubby@users.noreply.github.com>
2023-02-21 07:31:51 +1000

    invoice api test2

--- app/Managers/TransactionManager.php
+++ app/Managers/TransactionManager.php
@@ -99,7 +99,7 @@ class TransactionManager
         // Then send email to party with link
 
         // And return invoice itself to browser or have a different view
-        return $link;
+        return $invoice;
     }
 
     public function store(
