66070cd42914b020aa36d35e7577fbfed4903a63
nubby0208 <109526476+gruTonubby@users.noreply.github.com>
2023-02-21 21:51:58 +1000

    test

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -200,7 +200,7 @@ class AuthController extends BaseController
 	// $token = Hash::make($request->time);
     // $user->verification_token = $token;
     // $user->save();
-    $token = 'aaaaa'
+    $token = 'aaaaa';
 	$link = 'https://admin.dazu.pl/#/reset?token='.$token;
 	if($user) {
 		$template_data = ['emailBody'=>'ResetPassword', 'emailTitle'=>'Password reset', 'link' => $link];
