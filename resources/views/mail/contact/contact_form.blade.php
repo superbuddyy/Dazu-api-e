aae0c8df0f2bf519d50ab6dbe5550ef107f02ec4
nubby0208 <nubby0208@outlook.com>
2023-04-14 18:49:04 +0900

    test

--- resources/views/mail/contact/contact_form.blade.php
+++ resources/views/mail/contact/contact_form.blade.php
@@ -1,9 +1,11 @@
 @component('mail.layout')
     @lang('mail.contact_form_content')
-    {{ $name }}
-    <!-- {{ $name . ' - ' . $email }} -->
+    <!-- {{ $name . ' - ' . $email }}
     <br>
     <h3>Temat: {{ $topic }}</h3>
-    <p>{{ $message }}</p><br><br>
+    <p>{{ $message }}</p><br><br> -->
+    <br>
+    <h3>Temat: </h3>
+    <p>asdf</p><br><br>
     @lang('mail.contact_form_footer')
 @endcomponent
