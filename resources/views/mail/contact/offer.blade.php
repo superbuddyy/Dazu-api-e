451527e24c8a76a04711f7769387e7cd6b77f965
nubby0208 <nubby0208@outlook.com>
2023-04-15 10:33:43 +0900

    test

--- resources/views/mail/contact/offer.blade.php
+++ resources/views/mail/contact/offer.blade.php
@@ -1,6 +1,6 @@
 @component('mail.layout')
     @lang('mail.contact_offer_content')
-    <p>{{ $name . ' - ' . $email }}</p>
+    <p>{{ $name }} - {{ $email }}</p>
     <br>
     <p>{{ $message }}</p><br><br>
     @lang('mail.contact_offer_footer')
