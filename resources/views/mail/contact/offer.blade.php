d838ae25d069f78384a46da9f70db4e27be1c4d6
nubby0208 <nubby0208@outlook.com>
2023-04-16 16:04:08 +0900

    test

--- resources/views/mail/contact/offer.blade.php
+++ resources/views/mail/contact/offer.blade.php
@@ -7,8 +7,9 @@
   <body bgcolor="#11C9FF">
 <div class="container">
 	<div class="text-align">
-        	<h1 style="color:#334488">{{ $name . ' - ' . $email }}</h1>
-		<p>{{ $message }}</p><br/>
+        	<h1 style="color:#334488">{{$name}}</h1><br/>
+        	<h1 style="color:#334488">{{$email}}</h1>
+		<p>{{$message}}</p><br/>
 	</div>
 </div>  
 </body>
