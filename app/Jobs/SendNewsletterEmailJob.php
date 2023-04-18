b7aa38935d6958e884544dff82d9f504e5484bbc
nubby0208 <nubby0208@outlook.com>
2023-04-18 22:19:32 +0900

    fix

--- app/Jobs/SendNewsletterEmailJob.php
+++ app/Jobs/SendNewsletterEmailJob.php
@@ -14,6 +14,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 // use Illuminate\Support\Facades\Mail;
 use Mail;
