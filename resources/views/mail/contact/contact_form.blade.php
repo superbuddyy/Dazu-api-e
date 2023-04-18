57ad2665ff80f1edd6c3b173ba127889926b41cb
nubby0208 <nubby0208@outlook.com>
2023-04-19 05:00:33 +0900

    contact topic add

--- resources/views/mail/contact/contact_form.blade.php
+++ resources/views/mail/contact/contact_form.blade.php
@@ -7,6 +7,7 @@
   <body bgcolor="#11C9FF">
 <div class="container">
 	<div class="text-align">
+			<h1 style="color:#334488">{{$topic}}</h1><br/>
         	<h1 style="color:#334488">{{$name}}</h1><br/>
         	<h1 style="color:#334488">{{$email}}</h1>
 		<p>{{$messages}}</p><br/>
