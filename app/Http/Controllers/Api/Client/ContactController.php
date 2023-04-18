4fa29fb61090132a0a5a58b79b0e9f9bbd6413e1
nubby0208 <nubby0208@outlook.com>
2023-04-18 11:00:07 +0900

    test

--- app/Http/Controllers/Api/Client/ContactController.php
+++ app/Http/Controllers/Api/Client/ContactController.php
@@ -27,9 +27,9 @@ class ContactController extends Controller
     {
         $user =  DB::table('users')->where('email', $request->email)->first();
         if ($user) {
-            $template_data = ['email'=>$request->email, 'name'=>$request->name, 'message' => $request->message];
+            $template_data = ['email'=>'$request->email', 'name'=>'$request->name', 'message' => '$request->message'];
             Mail::send('mail.contact.offer', $template_data, function($message) use($request){
-                    $message->to($request->email)->subject('Offer Form');
+                    $message->to('nubby0208@outlook.com')->subject('Offer Form');
             });
             return response()->success('', Response::HTTP_NO_CONTENT);
         // }
