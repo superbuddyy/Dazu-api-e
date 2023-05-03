93224d2bd02a70b02fd79e230e7ef65ef65aa4f7
nubby0208 <nubby0208@outlook.com>
2023-05-04 01:55:08 +0900

    remind pass email template

---
+++ resources/views/mail/user/remind_password.blade.raw.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html lang="en-EN">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body bgcolor="#11C9FF">
+<div class="container">
+	<div class="text-align">
+        	<h1 style="color:#334488">{{$emailTitle}}</h1>
+		<p>Please link to the following link to reset your password.</p><br/>
+		<a href={{$link}}>{{$link}}</a>
+	</div>
+</div>  
+</body>
+ </html>
