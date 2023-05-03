ca3df5715721e0381c3390e00702be06eda4770b
nubby0208 <nubby0208@outlook.com>
2023-05-03 16:21:52 +0900

    fix

--- resources/views/mail/user/register.blade.php
+++ resources/views/mail/user/register.blade.php
@@ -1,16 +1,13 @@
-
-<!DOCTYPE html>
- <html lang="en-EN">
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body bgcolor="#11C9FF">
-<div class="container">
-	<div class="text-align">
-        	<h1 style="color:#334488">{{$emailTitle}}</h1>
-		<p>Please link to the following link to reset your password.</p><br/>
-		<a href={{$link}}>{{$link}}</a>
-	</div>
-</div>  
-</body>
- </html>
+@component('mail.layout')
+    @slot('support_email')
+        {{ 'change me' }}
+    @endslot
+    @slot('support_website')
+        {{ 'change me' }}
+    @endslot
+    @lang('mail.registration_content')<br>
+    @component('mail::button', ['url' => config('dazu.frontend_url').'/dokoncz-rejestracje?token='.$user['verification_token']])
+        @lang('mail.confirm_registration')
+    @endcomponent
+    @lang('mail.end_summary')<br>
+@endcomponent
