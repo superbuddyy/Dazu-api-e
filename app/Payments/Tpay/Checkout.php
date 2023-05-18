0c0d85d8dcc7eaf6ca4d77f2834c526f197217d5
nubby0208 <nubby0208@outlook.com>
2023-05-18 10:45:01 +0900

    fix

--- app/Payments/Tpay/Checkout.php
+++ app/Payments/Tpay/Checkout.php
@@ -26,6 +26,7 @@ class Checkout extends TransactionApi
         $real_refId = $splitRefId[0];
         $platform = $splitRefId[1];
 
+        $return_url = '';
         if($platform === 'desktop')
             $return_url = config('dazu.frontend_url') . '?payment-status=success';
         else if($platform === 'mobile')
