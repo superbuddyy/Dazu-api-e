7d6d2af55402a6234e386797e4431fa0a869f8f9
nubby0208 <nubby0208@outlook.com>
2023-05-23 10:18:51 +0900

    type

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -63,7 +63,7 @@ class UserResource extends JsonResource
                 },
                 $this->roles->toArray()
             ),
-            'type' => $role,
+            'type' => $role['type'],
             'permissions' => array_map(
                 function ($permission) {
                     return $permission['name'];
